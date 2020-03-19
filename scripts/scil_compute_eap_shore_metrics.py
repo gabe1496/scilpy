f598094c965b40180562df593bf57bb5345abe8a
Gabrielle Grenier <gabrielle.g.grenier@usherbrooke.ca>
2020-03-19 12:00:02 -0400

    Update the scripts with flake8 and new requirements

--- scripts/scil_compute_eap_shore_metrics.py
+++ scripts/scil_compute_eap_shore_metrics.py
@@ -1,13 +1,16 @@
 #! /usr/bin/env python
 
+"""
+Script to compute isotropic 3D-SHORE ODFs.
+See [Ozarslan et al NeuroImage 2013, Fick et al. 2014-2015].
+
+WARNING: need shore_ozarslan.py which is currently not in DIPY master.
+"""
+
 from __future__ import division, print_function
 
 import argparse
-import os
 import logging
-import warnings
-
-from ast import literal_eval
 
 import nibabel as nib
 import numpy as np
@@ -18,20 +21,15 @@ from dipy.data import get_sphere
 from dipy.io.gradients import read_bvals_bvecs
 
 from scilpy.reconst.shore_ozarslan import ShoreOzarslanModel
-from scilpy.io.utils import add_force_b0_arg
+from scilpy.io.utils import (add_overwrite_arg, assert_inputs_exist,
+                             assert_outputs_exist, add_force_b0_arg,
+                             add_sh_basis_args)
 from scilpy.utils.bvec_bval_tools import check_b0_threshold
-
-DESCRIPTION = """
-    Script to compute isotropic 3D-SHORE ODFs. 
-    See [Ozarslan et al NeuroImage 2013, Fick et al. 2014-2015].
-
-    WARNING: need shore_ozarslan.py which is currently not in DIPY master.
-    """
 
 
 def buildArgsParser():
 
-    p = argparse.ArgumentParser(description=DESCRIPTION,
+    p = argparse.ArgumentParser(description=__doc__,
                                 formatter_class=argparse.RawTextHelpFormatter)
 
     p.add_argument('input', action='store', metavar='input', type=str,
@@ -43,32 +41,28 @@ def buildArgsParser():
     p.add_argument('bvecs', action='store', metavar='bvecs',
                    help='Path of the bvecs file, in FSL format.')
 
-    p.add_argument('-f', action='store_true', dest='overwrite',
-                   help='If set, the saved files volume will be overwritten ' +
-                   'if they already exist.')
-
     p.add_argument('--radial_order', action='store', dest='radial_order',
                    metavar='int', default=8, type=int,
                    help='Radial order used for the SHORE fit. (Default: 8)')
 
     p.add_argument('--radial_moment', action='store', dest='radial_moment',
                    metavar='int', default=2, type=int,
-                   help='Radial moment used for the ODF reconstruction. (Default: 2)')
+                   help='Radial moment used for the ODF reconstruction. ' +
+                        '(Default: 2)')
 
     p.add_argument('--regul_weighting', action='store', dest='regul_weighting',
                    metavar='float', default=0.2, type=float,
-                   help='Laplacian weighting for the regularization. 0.0 will make the ' +
-                   'generalized cross-validation (GCV) kick in. (Default: 0.2)')
+                   help='Laplacian weighting for the regularization. '
+                        '0.0 will make the generalized cross-validation ' +
+                        '(GCV) kick in. (Default: 0.2)')
 
-    p.add_argument('--basis', action='store', dest='basis',
-                   metavar='string', default='fibernav',
-                   type=str, help='Basis used for the SH coefficients. Must ' +
-                                  'be either mrtrix or fibernav (default).')
+    add_sh_basis_args(p)
 
     p.add_argument('--mask', action='store', dest='mask',
                    metavar='', default=None, type=str,
                    help='Path to a binary mask. Only the data inside the ' +
-                        'mask will be used for computations and reconstruction.')
+                        'mask will be used for computations and ' +
+                        'reconstruction.')
 
     p.add_argument('--not_all', action='store_true', dest='not_all',
                    help='If set, only saves the files specified using the ' +
@@ -77,18 +71,22 @@ def buildArgsParser():
     g = p.add_argument_group(title='File flags')
     g.add_argument('--odf', action='store', dest='odf',
                    metavar='file', default='', type=str,
-                   help='Output filename for the ODF spherical harmonics coefficients.')
+                   help='Output filename for the ODF spherical harmonics ' +
+                   'coefficients.')
     g.add_argument('--rtop', action='store', dest='rtop',
                    metavar='file', default='', type=str,
-                   help='Output filename for the Return-To-Origin Probability (rtop).')
+                   help='Output filename for the Return-To-Origin ' +
+                   'Probability (rtop).')
     g.add_argument('--msd', action='store', dest='msd',
                    metavar='file', default='', type=str,
-                   help='Output filename for the Mean Squared Displacement (msd).')
+                   help='Output filename for the Mean Squared ' +
+                   'Displacement (msd).')
     g.add_argument('--pa', action='store', dest='pa',
                    metavar='file', default='', type=str,
                    help='Output filename for the Propagator Anisotropy (pa).')
 
     add_force_b0_arg(p)
+    add_overwrite_arg(p)
 
     return p
 
@@ -98,7 +96,7 @@ def main():
     parser = buildArgsParser()
     args = parser.parse_args()
     logging.basicConfig(level=logging.INFO)
-    
+
     if not args.not_all:
         if not args.odf:
             args.odf = 'shore_dodf.nii.gz'
@@ -115,12 +113,8 @@ def main():
         parser.error('When using --not_all, you need to specify at least ' +
                      'one file to output.')
 
-    for out in arglist:
-        if os.path.isfile(out):
-            if args.overwrite:
-                logging.info('Overwriting "{0}".'.format(out))
-            else:
-                parser.error('"{0}" already exists! Use -f to overwrite it.'.format(out))
+    assert_inputs_exist(parser, [args.input, args.bvals, args.bvecs])
+    assert_outputs_exist(parser, args, arglist)
 
     vol = nib.load(args.input)
     data = vol.get_data()
@@ -134,45 +128,49 @@ def main():
         mask = None
     else:
         mask = nib.load(args.mask).get_data().astype(np.bool)
-        voxels_with_values_mask = data[:,:,:,0]>0
+        voxels_with_values_mask = data[:, :, :, 0] > 0
         mask = voxels_with_values_mask * mask
 
     sphere = get_sphere('repulsion100')
 
     if args.regul_weighting <= 0:
-        logging.info('Now computing SHORE ODF of radial order {0}'.format(args.radial_order) +
+        logging.info('Now computing SHORE ODF of radial order {0}'
+                     .format(args.radial_order) +
                      ' and Laplacian generalized cross-validation')
 
         shore_model = ShoreOzarslanModel(gtab, radial_order=args.radial_order,
                                          laplacian_regularization=True,
                                          laplacian_weighting='GCV')
-    else :
-        logging.info('Now computing SHORE ODF of radial order {0}'.format(args.radial_order) +
-                     ' and Laplacian regularization weight of {0}'.format(args.regul_weighting))
+    else:
+        logging.info('Now computing SHORE ODF of radial order {0}'
+                     .format(args.radial_order) +
+                     ' and Laplacian regularization weight of {0}'
+                     .format(args.regul_weighting))
 
         shore_model = ShoreOzarslanModel(gtab, radial_order=args.radial_order,
                                          laplacian_regularization=True,
                                          laplacian_weighting=args.regul_weighting)
 
     smfit = shore_model.fit(data, mask)
-    odf = smfit.odf(sphere, radial_moment=args.radial_moment)    
-    odf_sh = sf_to_sh(odf, sphere, sh_order=8, basis_type=args.basis, smooth=0.0)    
+    odf = smfit.odf(sphere, radial_moment=args.radial_moment)
+    odf_sh = sf_to_sh(odf, sphere, sh_order=8, basis_type=args.sh_basis,
+                      smooth=0.0)
 
     rtop = smfit.rtop()
     msd = smfit.msd()
     pa = smfit.propagator_anisotropy()
-            
+
     if args.odf:
-        nib.save(nib.Nifti1Image(odf_sh.astype(np.float32), affine), args.odf)    
-    
+        nib.save(nib.Nifti1Image(odf_sh.astype(np.float32), affine), args.odf)
+
     if args.rtop:
-        nib.save(nib.Nifti1Image(rtop.astype(np.float32), affine), args.rtop)    
+        nib.save(nib.Nifti1Image(rtop.astype(np.float32), affine), args.rtop)
 
     if args.msd:
-        nib.save(nib.Nifti1Image(msd.astype(np.float32), affine), args.msd)    
+        nib.save(nib.Nifti1Image(msd.astype(np.float32), affine), args.msd)
 
     if args.pa:
-        nib.save(nib.Nifti1Image(pa.astype(np.float32), affine), args.pa)    
+        nib.save(nib.Nifti1Image(pa.astype(np.float32), affine), args.pa)
 
 
 if __name__ == "__main__":
